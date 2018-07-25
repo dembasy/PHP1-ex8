0b75a7d994f2e86c854202b2e2a8d841335e11f2
dembasy <dembasy1227@gmail.com>
2018-07-25 16:27:13 +0200

    done

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>ex8</title>
+  </head>
+  <body>
+    <p><?php $addition = 3 + 4; $multiply = 5 * 20 ; $dvsn = 45 / 5 ; echo $addition. $multiply. $dvsn; ?></p>
+  </body>
+</html>
